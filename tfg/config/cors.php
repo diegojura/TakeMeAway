1fde4954913f26292961060a27d23abe83c83df4
diegojura <154268497+diegojura@users.noreply.github.com>
2025-06-11 05:16:23 +0200

    raki 6.0 no corps

--- tfg/config/cors.php
+++ tfg/config/cors.php
@@ -1,31 +1,11 @@
 <?php
 
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    */
-
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
-
-    'allowed_methods' => ['*'],
-
-    // aquí pones tu front en prod y el localhost para dev
-    'allowed_origins' => [
-        env('FRONTEND_URL', 'http://localhost:5173'),
-        'https://delightful-kindness-production-e28c.up.railway.app',
-    ],
-
-    'allowed_origins_patterns' => [],
-
-    'allowed_headers' => ['*'],
-
-    'exposed_headers' => [],
-
-    'max_age' => 0,
-
-    'supports_credentials' => true,
+'paths'           => ['api/*', 'sanctum/csrf-cookie'],
+'allowed_methods' => ['*'],
+'allowed_origins' => ['http://localhost:5173'], // tu Vite dev
+'allowed_headers' => ['*'],
+'exposed_headers' => [],
+'supports_credentials' => true,
 
 ];

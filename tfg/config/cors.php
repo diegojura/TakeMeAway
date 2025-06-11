a170d3d2fccffc4e2f6333d384e7dea8faf60565
diegojura <154268497+diegojura@users.noreply.github.com>
2025-06-11 04:34:53 +0200

    raki 5.1 corp 2

--- tfg/config/cors.php
+++ tfg/config/cors.php
@@ -1,11 +1,31 @@
 <?php
 
 return [
-'paths'           => ['api/*', 'sanctum/csrf-cookie'],
-'allowed_methods' => ['*'],
-'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:5173')],
-'allowed_headers' => ['*'],
-'exposed_headers' => [],
-'supports_credentials' => true,
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cross-Origin Resource Sharing (CORS) Configuration
+    |--------------------------------------------------------------------------
+    */
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['*'],
+
+    // aquí pones tu front en prod y el localhost para dev
+    'allowed_origins' => [
+        env('FRONTEND_URL', 'http://localhost:5173'),
+        'https://delightful-kindness-production-e28c.up.railway.app',
+    ],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => true,
 
 ];
